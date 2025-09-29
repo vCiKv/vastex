102a8e064de1c25a24e2bd47a228dffeddd907d7
Kevin <54258753+vCiKv@users.noreply.github.com>
2025-09-29 14:58:48 +0100

    update: dialog

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,19 +1,21 @@
 "use client"
 import Link from "next/link"
+import React from "react"
 import Image from "next/image"
+import Section from "@/components/section"
+import useHotjar from "@/hooks/useHotjar"
+import SocialMedia from "@/components/social-media"
+import PageHeader from "@/components/page-header"
 import CallToAction from "@/components/cta"
 import ScrollReveal from "@/components/scroll-reveal"
-import Section from "@/components/section"
-import { partners, SocialMediaList } from "../companyDetails"
 import { Blob } from "@/components/ui/blob"
+import { toast } from "sonner"
 import { Button } from "@/components/ui/button"
-import { ArrowRight, ChevronRight, Copy, XIcon } from "lucide-react"
-import { FaPeopleGroup, FaListCheck, FaShieldHalved, FaMoneyBill1Wave, FaStar } from "react-icons/fa6";
-import { FaGlobeAfrica } from "react-icons/fa";
 import { RiFlagLine } from "react-icons/ri";
 import { TbContract } from "react-icons/tb";
-import PageHeader from "@/components/page-header"
-import { toast } from "sonner"
+import { partners, SocialMediaList } from "../companyDetails"
+import { ArrowRight, ChevronRight, Copy, XIcon } from "lucide-react"
+import { FaPeopleGroup, FaListCheck, FaShieldHalved, FaMoneyBill1Wave } from "react-icons/fa6";
 import {
   Dialog,
   DialogClose,
@@ -22,9 +24,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import SocialMedia from "@/components/social-media"
-import React from "react"
-import useHotjar from "@/hooks/useHotjar"
 
 
 function Person(props: {
@@ -69,35 +68,21 @@ function Person(props: {
             <p className="text-primary/80 font-light">{member.role}</p>
             <p className="text-primary font-bold">{member.department}</p>
           </div>
-          {/* {index > 1 && (<div>
-            <p className="lowercase font-light text-sm">Qualifications</p>
-            <div className="flex flex-row flex-wrap gap-1.5">
-              {member.qualifications.map(qualification =>
-                <span
-                  key={member.name + "-" + qualification}
-                  className="bg-primary/80 text-white rounded-lg px-1.5 pb-0.5"
-                >
-                  {qualification}
-                </span>
-              )}
-            </div>
-          </div>
-          )} */}
           <Dialog>
             <DialogTrigger>
               <span className="inline-flex flex-row flex-nowrap w-full gap-2 justify-center items-center text-primary cursor-pointer hover:bg-primary/80 hover:text-white py-0.5 px-3 rounded-full">see more <ChevronRight className="size-4" /></span>
             </DialogTrigger>
-            <DialogContent className="bg-transparent backdrop-blur-md  h-screen overflow-auto fixed rounded-2xl p-0" showCloseButton={false}>
-              <div className="relative size-full w-full md:max-w-[800px]">
+            <DialogContent hideClose className="bg-transparent backdrop-blur-md h-[90vh] md:h-[95vh] md:max-w-[800px] overflow-auto fixed rounded-2xl p-0 scroll-smooth border-transparent" >
+              <div className="relative size-full w-full ">
                 <DialogTitle className="hidden">leader </DialogTitle>
                 <DialogClose
                   data-slot="dialog-close"
-                  className="focus:ring-ring data-[state=open]:bg-accent  absolute top-4 right-4 opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-5 bg-red-600 text-white rounded-full p-1 shadow-md cursor-pointer"
+                  className="z-10 focus:ring-ring data-[state=open]:bg-accent  absolute top-4 right-4 opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-5 bg-red-600 text-white rounded-full p-1 shadow-md cursor-pointer"
                 >
                   <XIcon />
                   <span className="sr-only">Close</span>
                 </DialogClose>
-                <DialogHeader className="mx-auto text-center bg-accent/90 w-full pt-6 pb-10">
+                <DialogHeader className="mx-auto text-center bg-accent/90 w-full pt-4 pb-6 md:pt-6 md:pb-10">
                   <div className="relative aspect-square size-64 md:size-80 rounded-2xl overflow-hidden mx-auto">
                     <Image
                       src={member.avatarUrl}
@@ -132,24 +117,7 @@ function Person(props: {
                     <SocialMedia socials={member.contact.socials ?? {}} className="px-3.5 py-0.5 w-min text-primary backdrop-blur-2xl bg-primary/10 justify-center mx-auto rounded-md" iconClassName="text-primary size-5" />
                   </div>
                 </DialogHeader>
-                <div className="space-y-3 px-6 pt-6 pb-10 bg-white">
-                  {/* <div>
-                    <p className="lowercase font-light text-sm">Qualifications</p>
-                  
-                  </div> */}
-                  {/* <div>
-                    <p className="lowercase font-light text-sm">Experiences</p>
-                    <div className="flex flex-row flex-wrap gap-1.5 ">
-                      {member.experience.map(experience =>
-                        <span
-                          key={member.name + "-" + experience}
-                          className="bg-primary/80 text-white rounded-lg px-1.5 py-0.5"
-                        >
-                          {experience}
-                        </span>
-                      )}
-                    </div>
-                  div> */}
+                <div className="space-y-3 px-4 pt-4 md:px-6 md:pt-6 pb-10 bg-white">
                   <div>
                     <p className="lowercase font-light text-sm">Bio</p>
                     {/* <p className="text-base text-left whitespace-pre-line"> */}
@@ -160,58 +128,15 @@ function Person(props: {
                       ))}
                     </div>
                   </div>
-
-                  {/* <div>
-                    <p className="lowercase pt-4 font-light text-sm">Contact</p>
-                    <p className="text-primary inline-flex gap-2 cursor-pointer items-center" onClick={() => { navigator.clipboard.writeText(member.contact.companyEmail); toast.success("Email copied to clipboard") }}>
-                      {member.contact.companyEmail} <Copy />
-                    </p>
-                  </div> */}
                 </div>
               </div>
-
             </DialogContent>
           </Dialog>
-          {/* <span onClick={toggle} className="inline-flex flex-row flex-nowrap w-full gap-2 justify-center items-center text-primary cursor-pointer">see {!showBio ? <>more <ChevronDown className="size-4" /></> : <>less <ChevronUp className="size-4" /></>}</span> */}
-          {/* {showBio && <>
-            <p className="text-base text-left">
-              {member.bio}
-            </p>
-            <div>
-              <p className="lowercase pt-4 font-light text-sm">Experiences</p>
-              <div className="flex flex-row flex-wrap gap-1.5">
-                {member.experience.map(experience =>
-                  <span
-                    key={member.name + "-" + experience}
-                    className="bg-primary/80 text-white rounded-lg px-1.5 py-0.5"
-                  >
-                    {experience}
-                  </span>
-                )}
-              </div>
-            </div>
-            <div>
-              <p className="lowercase pt-4 font-light text-sm">Contact</p>
-              <p className="text-primary inline-flex gap-2 cursor-pointer items-center" onClick={() => { navigator.clipboard.writeText(member.contact.companyEmail); toast.success("Email copied to clipboard") }}>
-                {member.contact.companyEmail} <Copy />
-              </p>
-            </div>
-            <span onClick={toggle} className="inline-flex flex-row flex-nowrap w-full gap-2 justify-center items-center text-primary cursor-pointer">see {!showBio ? <>more <ChevronDown className="size-4" /></> : <>less <ChevronUp className="size-4" /></>}</span>
-          </>} */}
           <div>
-            {/* <p className="lowercase pt-4 font-light text-sm">Contact</p>
-            <div className="flex flex-row flex-wrap gap-1.5">
-              {member.contact.phones.map(number =>
-                <span key={number} className="inline-flex gap-1">
-                  <Phone />
-                  <span>{number}</span>
-                </span>
-              )}
-            </div> */}
           </div>
         </div>
       </div >
-    </ScrollReveal >
+    </ScrollReveal>
   )
 }
 export default function AboutPage() {
@@ -237,21 +162,14 @@ export default function AboutPage() {
           />
           <Blob variant="dots" color="text-gray-400" size="xl" className="absolute right-0 top-0 z-0" opacity={0.3} animate />
           <div className="container relative z-10 px-4 md:px-6 pt-8">
-            <div className="grid gap-6 grid-col-2 lg:grid-cols-3 lg:gap-12 xl:gap-16">
+            <div className="grid gap-6 grid-col-2 md:grid-cols-3 lg:gap-12 xl:gap-16">
               <ScrollReveal direction="left" delay={300} className="md:col-span-2">
                 <div className="flex flex-col justify-center space-y-4 md:pb-12">
                   <div className="space-y-2 pb-4">
                     <div className="inline-block rounded-xl bg-blue-100 items-center justify-center px-3 py-1 text-sm text-primary">How We Help You</div>
-                    {/* <h1 className="text-3xl font-bold tracking-tighter text-primary sm:text-4xl md:text-5xl">
-                      Our Story
-                    </h1> */}
                     <h1 className="text-3xl font-bold tracking-tighter text-primary sm:text-4xl md:text-5xl">
                       About Us
                     </h1>
-                    {/* <p className="max-w-[600px] text-gray-600 md:text-xl ">
-                      VASTEX Resources Limited was founded to provide services
-                      that drive growth and efficiency for our clients.
-                    </p> */}
                   </div>
                   <p className="pb-24 pr-6">
                     VASTEX Resources Limited is an integrated services firm dedicated to assisting businesses achieve their commercial objectives, with focus on Business Turnaround /Operations Advisory, Company Representation and New Business Setup.
@@ -278,12 +196,12 @@ export default function AboutPage() {
         // }}
         >
           <div className="grid grid-cols-1 px-6 md:px-8 gap-12 pb-12 size-full">
-            <div className="border-0 rounded-xl py-2 group">
+            <div className="border-0 rounded-xl py-2 group bg-white md:w-fit">
               <div className="flex  items-center gap-x-6 border-l-4 group-hover:border-primary">
                 <div className="flex justify-center items-center p-2.5 ml-2.5 size-12 bg-accent text-primary group-hover:bg-primary group-hover:text-accent rounded-full">
                   <RiFlagLine className="size-full" />
                 </div>
-                <div className="py-2">
+                <div className="py-2 ">
                   <h4 className="group-hover:text-primary text-3xl font-bold tracking-tight font-gilroy pb-4">Our Mission</h4>
                   <p className="group-hover:text-primary/60 leading-5">To be a pivotal partner to all our clients</p>
                 </div>
@@ -315,26 +233,21 @@ export default function AboutPage() {
                   icon: <FaMoneyBill1Wave className="text-primary" />,
                   title: "Market Knowledge",
                   description: "We know the market"
-                  // "is the strategic foresight that enables us to pinpoint opportunities, navigate competition, and deliver measurable impact.",
                 },
                 {
-                  // icon: <FaGlobeAfrica className="text-primary" />,
                   icon: <FaPeopleGroup className="text-primary" />,
                   title: "Hands-on",
                   description: "Our strength is in brilliant implementation"
-                  // "Propelling forward ideas, ensuring our solutions consistently redefine industry benchmarks and secure future relevance.",
                 },
                 {
                   icon: <FaListCheck className="text-primary" />,
                   title: "Competence",
                   description: "We are experts at what we do"
-                  // "Our demonstrable capability and consistent delivery of superior results",
                 },
                 {
                   icon: <TbContract className="text-primary" />,
                   title: "Partnership",
                   description: "We execute our briefs as if we are invested in the enterprise"
-                  // "Aligning objectives and amplifying collective strengths for unparalleled success.",
                 },
                 {
                   icon: <FaShieldHalved className="text-primary" />,
@@ -342,20 +255,6 @@ export default function AboutPage() {
                   description:
                     "We keep our word",
                 },
-                // {
-                //   icon: <FaStar className="text-primary" />,
-                //   title: "Passion",
-                //   description:
-                //     "The bedrock of all successful engagements",
-                // },
-                // {
-                //   icon: <FaPeopleGroup className="text-primary" />,
-                //   title: "Relationship",
-                //   description:
-                //     "Transforming transactional interactions into invaluable, long-term strategic alliances.",
-                // },
-
-
               ].map((value, index) => (
                 <ScrollReveal key={index} delay={100 * (index + 1)} direction="up" className="h-full">
                   <div className="relative h-full">
@@ -401,37 +300,6 @@ export default function AboutPage() {
           </div>
 
         </Section>
-
-        {/* CTA Section */}
-        {/* <CallToAction>
-          <Blob
-            variant="blob2"
-            color="text-white"
-            size="xl"
-            className="absolute -left-40 top-0 z-0"
-            opacity={0.07}
-            animate
-          />
-          <Blob variant="dots" color="text-white" size="lg" className="absolute right-0 bottom-0 z-0" opacity={0.1} />
-          <div className="container relative z-10 px-4 md:px-6">
-            <ScrollReveal>
-              <div className="flex flex-col items-center justify-center space-y-4 text-center">
-                <div className="space-y-2">
-                  <h2 className=" text-white">Join Our Team</h2>
-                  <p className="max-w-[900px] text-blue-100">
-                    {"We're"} always looking for talented individuals to join our growing team.
-                  </p>
-                </div>
-                <div className="flex flex-col gap-2 min-[400px]:flex-row">
-                  <Link href="/careers">
-                    <Button className="bg-white text-primary hover:bg-blue-50 transition-transform hover:scale-105">
-                      View Open Positions <ArrowRight className="ml-2 h-4 w-4" />
-                    </Button>
-                  </Link>
-                </div>
-              </div>
-            </ScrollReveal>
-          </div> */}
         <CallToAction>
           <div className="space-y-4 text-center">
             <h6 className="text-3xl font-bold tracking-tighter text-white sm:text-4xl">
@@ -459,7 +327,6 @@ export default function AboutPage() {
           </div>
         </CallToAction>
       </main>
-
-    </div >
+    </div>
   )
 }
